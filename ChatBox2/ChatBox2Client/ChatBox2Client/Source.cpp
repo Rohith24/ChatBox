90f21f2cda3a07749da527ed144201c422e177ab
Rohith24 <rohithreddy24@hotmail.com>
2016-07-02 16:39:22 +0530

    Submit

--- ChatBox2/ChatBox2Client/ChatBox2Client/Source.cpp
+++ ChatBox2/ChatBox2Client/ChatBox2Client/Source.cpp
@@ -1,27 +1,48 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+
 void main(){
 	FILE *f = fopen("F:\\GIT\\ChatBox\\ChatBox2\\source.bin", "rb+");
 	char s[256];
+	int no;
+	memset(s, '\0', 256);
 	printf("Enter msg:");
 	gets(s);
-	fputs(s, f);
+	fseek(f, 0, 0);
+	fread(&no, sizeof(int), 1, f);
+	no++;
+	fseek(f, 0, 0);
+	fwrite(&no, sizeof(int), 1, f);
+	fflush(f);
+	fseek(f, (no - 1) * 256 + 4, 0);
+	fwrite(s, 256, 1, f);
 	fflush(f);
 	while (1)
 	{
 		char str[256];
+		memset(str, '\0', 256);
+		fseek(f, 0, 0);
+		fread(&no, sizeof(int), 1, f);
+		if (no % 2 == 1) continue;
+		fseek(f, (no-1) * 256 + 4, 0);
 		if (fgets(str, 256, f) == 0)
 			continue;
-		if (str[0] == -52) continue;
+		if (str[0] == '\0') continue;
 		printf("Server: %s\n", str);
-		str[0] = -52;
 		char s[256];
+		memset(s, '\0', 256);
 		printf("Enter msg:");
 		gets(s);
-		//scanf("%s", s);
-		fputs(s, f);
+		fseek(f, 0, 0);
+		fread(&no, sizeof(int), 1, f);
+		no++;
+		fseek(f, 0, 0);
+		fwrite(&no, sizeof(int), 1, f);
 		fflush(f);
-		//fflush(f);
+		fseek(f, (no - 1) * 256 + 4, 0);
+		fwrite(s, 256, 1, f);
+		fflush(f);
 	}
 	fclose(f);
 	getchar();
